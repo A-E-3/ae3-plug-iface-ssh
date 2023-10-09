bee62b9efcfe6857b5b0036a17ba5a077f219abe
Alex Kharichev <myx@myx.nz>
2023-10-09 12:57:20 +0300

    * legacy myx.ru -> myx.co.nz|myx.nz

--- java/ru/myx/iface/ssh/KexCompression.java
+++ java/ru/myx/iface/ssh/KexCompression.java
@@ -5,33 +5,34 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 enum KexCompression {
+	
 	/**
-	 * 
+	 *
 	 */
-	GZIP("gzip@myx.ru", false) {
-	// ignore
+	GZIP("gzip@myx.co.nz", false) {
+		// ignore
 	},
 	/**
-	 * 
+	 *
 	 */
 	ZLIB("zlib", false) {
-	// ignore
+		// ignore
 	},
 	/**
-	 * 
+	 *
 	 */
 	NONE("none", true) {
-	// ignore
+		// ignore
 	},
 	//
 	;
-	
-	private final static Map<String, KexCompression>	SUPPORTED_MAP;
-	
+
+	private final static Map<String, KexCompression> SUPPORTED_MAP;
+
 	/**
-	 * 
+	 *
 	 */
-	public final static byte[]							KEX;
+	public final static byte[] KEX;
 	static {
 		final Map<String, KexCompression> supportedMap = new HashMap<>();
 		final StringBuilder supportedString = new StringBuilder();
@@ -40,30 +41,32 @@ enum KexCompression {
 				continue;
 			}
 			if (supportedString.length() > 0) {
-				supportedString.append( ',' );
+				supportedString.append(',');
 			}
-			supportedString.append( current.name );
-			supportedMap.put( current.name, current );
+			supportedString.append(current.name);
+			supportedMap.put(current.name, current);
 		}
 		SUPPORTED_MAP = supportedMap;
 		KEX = supportedString.toString().getBytes();
 	}
-	
+
 	final static KexCompression chooseKexCompression(final String kexCompressions) {
-		for (final StringTokenizer st = new StringTokenizer( kexCompressions, "," ); st.hasMoreTokens();) {
-			final KexCompression kexCompression = KexCompression.SUPPORTED_MAP.get( st.nextToken() );
+		
+		for (final StringTokenizer st = new StringTokenizer(kexCompressions, ","); st.hasMoreTokens();) {
+			final KexCompression kexCompression = KexCompression.SUPPORTED_MAP.get(st.nextToken());
 			if (kexCompression != null) {
 				return kexCompression;
 			}
 		}
 		return null;
 	}
-	
-	private final String	name;
-	
-	private final boolean	supported;
-	
+
+	private final String name;
+
+	private final boolean supported;
+
 	KexCompression(final String name, final boolean supported) {
+		
 		this.name = name;
 		this.supported = supported;
 	}
